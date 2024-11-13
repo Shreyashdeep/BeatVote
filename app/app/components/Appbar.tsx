b1604a4e3bd6590ce81858380858ad3050e9912a
Shreyashdeep <shreyashdeep20@gmail.com>
2024-11-13 23:08:21 +0530

    implement /dashboard

--- app/app/components/Appbar.tsx
+++ app/app/components/Appbar.tsx
@@ -1,18 +1,21 @@
 "use client";
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Music, Play, Users } from "lucide-react"
+import Link from "next/link"
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Appbar(){
     const session=useSession();
     
     return(
-        <div className="flex justify-between">
-            <div>
-                BeatVote
-            </div>
-            <div>
-                {session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signOut()}>Logout</button>}
-                {!session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signIn()}>SignIn</button>}
-            </div>
+        <div className='flex justify-between px-20 pt-4'>
+          <div className='text-lg font-bold flex flex-col justify-center text-white'>
+            BeatVote
+          </div>
+                {session.data?.user && <button className=" py-6 md:py-4 lg:py-12 xl:py-6 bg-gradient-to-b from-gray-900 " onClick={() => signOut()}>Logout</button>}
+                {!session.data?.user && <button className=" py-6 md:py-4 lg:py-12 xl:py-6 bg-gradient-to-b from-gray-900 " onClick={() => signIn()}>SignIn</button>}
         </div>
 
     );
